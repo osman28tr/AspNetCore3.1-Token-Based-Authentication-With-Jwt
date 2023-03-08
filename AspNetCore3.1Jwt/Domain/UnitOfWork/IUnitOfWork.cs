5554138e62fe5e3f10a0482d4051cef02a99ebc3
osman28tr <osmantonbul2001@gmail.com>
2023-03-08 16:52:37 +0300

    UnitOfWork tasarım deseni uygulandı.

---
+++ AspNetCore3.1Jwt/Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AspNetCore3._1Jwt.Domain.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task CompleteAsync();
+    }
+}
---
+++ AspNetCore3.1Jwt/Domain/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using AspNetCore3._1Jwt.Domain.Contexts;
+using System.Threading.Tasks;
+
+namespace AspNetCore3._1Jwt.Domain.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly UdemyApiWithTokenDBContext _context;
+        public UnitOfWork(UdemyApiWithTokenDBContext context)
+        {
+            _context = context;
+        }
+        public async Task CompleteAsync()
+        {
+            await this._context.SaveChangesAsync();
+        } 
+    }
+}
