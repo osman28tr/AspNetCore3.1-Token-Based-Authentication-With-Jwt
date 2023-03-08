134349c721db07333e50debc036f36bf9523e604
osman28tr <osmantonbul2001@gmail.com>
2023-03-08 17:53:28 +0300

    Product için service mekanizmasının soyutu oluşturuldu.

---
+++ AspNetCore3.1Jwt/Domain/Service/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCore3._1Jwt.Domain.Models;
+using AspNetCore3._1Jwt.Domain.Response;
+using System.Threading.Tasks;
+
+namespace AspNetCore3._1Jwt.Domain.Service
+{
+    public interface IProductService
+    {
+        Task<ProductListResponse> ListAsync();
+        Task<ProductResponse> AddProduct();
+        Task<ProductResponse> UpdateProduct(int productId, Product product);
+        Task<ProductResponse> DeleteProduct(int productId);
+        Task<Product> FindByIdAsync(int productId);
+    }
+}
